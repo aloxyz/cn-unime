80cac02ac1c61aa7b8e30711f334e8bd1b5d2658
Cestnut <hunterkudo@gmail.com>
2021-05-03 20:31:13 +0200

    reformatted exercise 3

--- Group 1/3 - multiplo.c
+++ Group 1/3 - multiplo.c
@@ -1,12 +1,22 @@
-//programma che dati due numeri A e B, dice se A è pari e se è multiplo di B
+// programma che dati due numeri A e B, dice se A è pari e se è multiplo di B
 #include <stdio.h>
+#include <stdlib.h>
 
-#define ISMULTIPLE(FORMAT, A, B)                                               \
-  if (A % 2) printf(FORMAT " e' dispari", A);                                  \
-  else       printf(FORMAT " e' pari", A);                                     \
+#define ISMULTIPLE(FORMAT, TYPE, V, SIZE)                                      \
+  if (SIZE == 1) {                                                             \
+    scanf(FORMAT, &V[0].TYPE);                                                 \
+    if (V[0].TYPE % 2)                                                         \
+      printf(FORMAT " e' dispari", V[0].TYPE);                                 \
+    else                                                                       \
+      printf(FORMAT " e' pari", V[0].TYPE);                                    \
+  }                                                                            \
+  if (SIZE == 2)                                                               \
+    scanf(FORMAT FORMAT, &V[0].TYPE, &V[1].TYPE);                              \
                                                                                \
-  if (A % B) printf(" e non e' multiplo di " FORMAT, B);                       \
-  else       printf(" ed e' multiplo di " FORMAT, B);
+  if (V[0].TYPE % V[1].TYPE)                                                   \
+    printf(FORMAT " non e' multiplo di " FORMAT, V[0].TYPE, V[1].TYPE);         \
+  else                                                                         \
+    printf(FORMAT " e' multiplo di " FORMAT, V[0].TYPE, V[1].TYPE);
 
 int main() {
 
@@ -15,23 +25,26 @@ int main() {
     int integer;
   } integer;
 
-  integer a, b;
-  short int choice;
+  integer *V;
+  short int choice, size;
   printf("1 per short int (2 byte), 2 per int (4 byte): ");
   scanf("%hd", &choice);
+  printf("Inserire 1 per vedere se il numero e' pari, inserire 2 per vedere se "
+         "il primo numero e' multiplo del secondo\n");
+  scanf("%hd", &size);
+if(size != 1 && size != 2) {
+  printf("default a 2\n"); 
+  size = 2;
+  }
   
   if (choice == 1) {
-    
+    V = malloc(sizeof(short) * size);
     printf("Inserire il primo e il secondo numero: ");
-    scanf("%hd%hd", &a.short_int, &b.short_int);
-    ISMULTIPLE("%hd", a.short_int, b.short_int)
-  } 
-  else {
-    if (choice != 2)    printf("Numero errato, default a int");
-    
-    printf("Inserire il primo e il secondo numero: ");
-    scanf("%d%d", &a.integer, &b.integer);
-
-    ISMULTIPLE("%d", a.integer, b.integer);
+    ISMULTIPLE("%hd", short_int, V, size);
+  } else {
+    if (choice != 2)
+      printf("Numero errato, default a int");
+    V = malloc(sizeof(int) * size);
+    ISMULTIPLE("%d", integer, V, size);
   }
 }
