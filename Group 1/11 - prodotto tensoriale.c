35dc76b6ef7473f001e836840ae0bbdb73c01550
aloxyz <aloxyz@tuta.io>
2021-05-04 21:10:16 +0200

    exercise 11 reformat

--- Group 1/11 - prodotto tensoriale.c
+++ Group 1/11 - prodotto tensoriale.c
@@ -1,37 +1,12 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "../CN.h"
 
-#define TENSOR_PROD(FORMAT, TYPE)\
-    v = malloc(sizeof(v->TYPE)*size + sizeof(tmp.TYPE));\
-    w = malloc(sizeof(w->TYPE)*size + sizeof(tmp.TYPE));\
-    printf("inizializzare il vettore 1\n>> ");\
-    for(int i = 0; i < size; i++) scanf(FORMAT, &v[i].TYPE);\
-    printf("inizializzare il vettore 2\n>> ");\
-    for(int i = 0; i < size; i++) scanf(FORMAT, &w[i].TYPE);\
-    printf("prodotto tensoriale:\n");\
-    printf("( ");\
-    for(int i = 0; i < size; i++)\
-        for(int j = 0; j < size; j++) printf(FORMAT " ", v[i].TYPE * w[j].TYPE);\
-    printf(" )");
+int main(){
+    Matrix *A, *B, *C;
+    A = prompt_matrix();
+    B = prompt_matrix();
+    C = v_tensor_prod(A,B);
+    if(C == NULL) printf("Matrici non compatibili per il prodotto scalare\n");
+    else print_matrix(C);
 
-int main() {
-    union {
-        short int short_int;
-        int integer;
-        float floating;
-        double double_prec;
-        char *word;
-    } *v, *w, tmp;
-
-    int t, size;
-    
-    printf("scegliere tra\n1. short_int\n2. integer\n3. floating\n4. double_prec\n>> ");
-    scanf("%d", &t);
-    printf("inserire la lunghezza dei vettori\n>>  ");
-    scanf("%d", &size);
-
-    if(t == 1) {TENSOR_PROD("%hd", short_int)} else
-    if(t == 3) {TENSOR_PROD("%f", floating)} else
-    if(t == 4) {TENSOR_PROD("%lf", double_prec)}
-    else       {TENSOR_PROD("%d", integer)}
+    return 0;
 }
