bd827742ca91f0e40156c4e5b01a0f3076af7972
Cestnut <hunterkudo@gmail.com>
2021-05-24 18:18:38 +0200

    Esercizio 1 gruppo 2 svolto

---
+++ Group 2/1 - Corollario teorema di equivalenza vettori.c
@@ -0,0 +1,33 @@
+/*Verifica il corollario del teorema di equivalenza delle norme vettoriali:
+norm_inf(v) <= norm2(v) <= sqrt(n)*norm_int(v)
+norm2(v) <= norm1(v) <= sqrt(n)*norm2(v)  
+norm_inf(v) <= norm1(v) <= n*norm_int(v)  
+
+per un generico vettore in input di n componenti
+*/
+
+#include "..\CN.h"
+
+void verify(double normA, double normB, double scalar);
+
+int main(){
+    int n;
+    printf("Inserire quanti elemenit vuoi il vettore abbia: ");
+    scanf("%d", &n);
+    Matrix *A = new_matrix("A", 1, n, double_prec);
+    prompt_init_elements(A);
+
+    verify(norm_inf(A, double_prec)->elements.double_prec[0], norm2(A, double_prec)->elements.double_prec[0], sqrt(size(A)));
+    verify(norm2(A, double_prec)->elements.double_prec[0], norm1(A, double_prec)->elements.double_prec[0], sqrt(size(A)));
+    verify(norm_inf(A, double_prec)->elements.double_prec[0], norm1(A, double_prec)->elements.double_prec[0], size(A));
+    
+    return 0;
+}
+
+void verify(double normA, double normB, double scalar){
+
+    printf("%.lf <= %lf <= %lf * %lf\n", normA, normB, scalar, normA);
+    if(normA <= normB && normB <= scalar * normA) printf("Vero\n");
+    else printf("Falso\n");
+
+}
